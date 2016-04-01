8b9b89ce228bf1b199a79e2af9d7032bff743d84
reality <tinmachin3@gmail.com>
2016-04-01 21:42:45 +0000

    thing

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,7 @@ function renderText(content) {
   var splitFile = fileName.split('.');
   var ext = splitFile[splitFile.length-1].toLowerCase();
   var a = $('#downloadButton');
-  $('#content').text(content);
+  $('#content').html(htmlentities(content));
   $('code').each(function(i, block) { //lol
     block.className = ext;
     if(ext == 'txt') {
